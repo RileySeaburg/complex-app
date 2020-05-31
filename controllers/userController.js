cf825b052d10ae927ec953bde5e3fea52c4b8aea
Riley Seaburg <riley@rds.marketing>
2020-05-31 10:33:32 -0400

    add user authentification & login

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+
+exports.login = function (req, res) {
+  let user = new User(req.body);
+  user.login(function (result) {
+    res.send(result);
+  });
+};
+
+exports.logout = function () {};
+
+exports.register = function (req, res) {
+  /* Capitalize the blueprint I.e. "User"*/
+  let user = new User(req.body);
+  user.register();
+  if (user.errors.length) {
+    res.send(user.errors);
+  } else {
+    res.send("Congrats, there are no errors.");
+  }
+};
+
+exports.home = function (req, res) {
+  res.render("home-guest");
+};
